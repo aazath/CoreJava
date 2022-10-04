9cdd6f497a851066114896edd6ea470dbe0f7905
aazath <asaazath@gmail.com>
2022-10-04 15:50:54 +0530

    equalsIgnorecase

--- StringOperation.java
+++ StringOperation.java
@@ -6,8 +6,12 @@ public class StringOperation {
 		
 		String s1 = "Alien";
 		String s2 = "Alien";
+		String s3 = "alien";
 		
 		System.out.println(s1==s2);
+		System.out.println(s1==s3);
+		System.out.println(s1.equals(s3));
+		System.out.println(s1.equalsIgnoreCase(s3));
 	}
 
 }
